f3fc86fe6eae58f314559b5f9e6ad868b735d29c
EC2 Default User <ec2-user@ip-10-10-47-99.us-west-2.compute.internal>
2021-07-15 12:53:07 +0000

    update setup

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 setup(
-  name = "SimpleEgg",
-  packages = find_packages()
-  )
+  name="SimpleEgg",
+  version="0.1",
+  description="Simple example calling Spark from a custom package",
+  license='MIT',
+  url='https://github.com/curtishoward/SimpleEgg',
+  packages=['SimpleEgg']
+)
