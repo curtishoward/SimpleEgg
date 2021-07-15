d9d3f7be8f8a5e7bc8dbf6b3bbe54f3054acabeb
EC2 Default User <ec2-user@ip-10-10-47-99.us-west-2.compute.internal>
2021-07-15 02:16:19 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from SimpleEgg import spark_session 
+
+if __name__ == '__main__':
+  spark_session.run()
+
---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, find_packages
+setup(
+  name = "SimpleEgg",
+  packages = find_packages()
+  )
